7f155d0ec4e40c1b9d1191bbc948406af1e74108
Andy Piloto <pilotoandy30@gmail.com>
2022-12-18 15:53:09 -0500

    un cambio ahi pendejo

--- src/main/java/cu/edu/cujae/pweb/dto/UserAuthenticatedDto.java
+++ src/main/java/cu/edu/cujae/pweb/dto/UserAuthenticatedDto.java
@@ -1,20 +1,24 @@
 package cu.edu.cujae.pweb.dto;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class UserAuthenticatedDto {
 
-    private int code;
+	private int code;
     private String username;
     private String password;
     private String email;
     private String recoverCode;
     private String firstName;
     private String lastName;
-    private List<RoleDto> roles;
-    private List<String> rolesName = new ArrayList<>();
+    private Collection<RoleDto> roles;
     private String token;
+
+    public UserAuthenticatedDto() {
+
+    }
 
     public UserAuthenticatedDto(int code,
                                 String username,
@@ -23,9 +27,9 @@ public class UserAuthenticatedDto {
                                 String recoverCode,
                                 String firstName,
                                 String lastName,
-                                List<RoleDto> roles,
-                                List<String> rolesName,
+                                Collection<RoleDto> roles,
                                 String token) {
+        super();
         this.code = code;
         this.username = username;
         this.password = password;
@@ -34,67 +38,77 @@ public class UserAuthenticatedDto {
         this.firstName = firstName;
         this.lastName = lastName;
         this.roles = roles;
-        this.rolesName = rolesName;
         this.token = token;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public int getCode() {
         return code;
     }
+
     public void setCode(int code) {
         this.code = code;
     }
-    public String getUsername() {
-        return username;
-    }
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public String getRecoverCode() {
         return recoverCode;
     }
+
     public void setRecoverCode(String recoverCode) {
         this.recoverCode = recoverCode;
     }
+
     public String getFirstName() {
         return firstName;
     }
+
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
+
     public String getLastName() {
         return lastName;
     }
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-    public List<RoleDto> getRoles() {
+
+    public Collection<RoleDto> getRoles() {
         return roles;
     }
-    public void setRoles(List<RoleDto> roles) {
+
+    public void setRoles(Collection<RoleDto> roles) {
         this.roles = roles;
     }
-    public List<String> getRolesName() {
-        return rolesName;
-    }
-    public void setRolesName(List<String> rolesName) {
-        this.rolesName = rolesName;
-    }
+
     public String getToken() {
         return token;
     }
+
     public void setToken(String token) {
         this.token = token;
     }
